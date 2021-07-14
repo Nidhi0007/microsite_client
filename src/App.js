2a9c9e71630031fea123955e56ab70310d00bdd5
Nidhi Bhat <bhatnidhis999@gmail.com>
2021-07-14 15:58:08 +0530

    react-ga

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Main from "./Layout/Main";
 import Store from "./Layout/Store";
+import ReactGA from "react-ga";
 
 function App() {
+  // Initializing GA and Tracking Pageviews
+
+  ReactGA.initialize("UA-000000-01");
+  ReactGA.pageview(window.location.pathname + window.location.search);
   return (
     <>
       <BrowserRouter>
